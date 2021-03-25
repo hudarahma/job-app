50ac34d4f4683b9c03cb250309d9289b755e3b84
Orkid <mrs.emadi@yahoo.com>
2021-03-25 14:07:26 +0430

    ADD:api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React , {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState();
+
+  useEffect(()=>{
+ 
+    fetch(
+      "https://jobs.github.com/positions.json?search=node"
+    )
+    .then(res => res.json())
+    .then(data => console.log(data));
+   
+    
+  },[]);
+  console.log(title);
+
+  // const getTitle = (data) => {
+  //   data.forEach((data, i) => {
+  //     console.log(data.title, i);
+  //   });
+  // }
+  
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <li>{title}</li>
     </div>
   );
 }
