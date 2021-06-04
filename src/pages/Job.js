9050a377ff1e7d655c401c4a96c8a85aa6d4545e
Orkid <mrs.emadi@yahoo.com>
2021-06-05 00:19:26 +0430

    ADD:job-component

--- src/pages/Job.js
+++ src/pages/Job.js
@@ -27,17 +27,14 @@ const Job = ({match, history}) => {
             <Header />
             { data && (
                 <div className={styles.wrapper_info}>
-
                     <div className={styles.site}>
                         <img  className={styles.logo} src={data.company_logo}/>
                         <div className={styles.center_info}>
                             <h2 className={styles.title}>{data.title}</h2>
                             <h4>{data.company}</h4>
                         </div>
-                        
                         <Button>Company Site</Button>
                     </div>
-
                     <div className={styles.body_wrapper}>
                         <div className={styles.company_info_header}>
                             <div className={styles.info}>
@@ -49,19 +46,14 @@ const Job = ({match, history}) => {
                             <Button onClick={() => window.open(data.url, '_blank')}>Apply</Button> 
                         </div>
                             
-                        
                         <div className={styles.company_info_body} {...html(data.description)}></div>
                     </div>
-
-
                     <div className={styles.how_to_apply}>
                         <span>How to Apply</span>
                         <span className={styles.apply} {...html(data.how_to_apply)}></span>
                     </div>
                 </div>
             )}
-            
-            
         </div>
     )
 }
